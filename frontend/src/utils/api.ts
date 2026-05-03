cc405439100c180ffdb87cba2106ee77d6617306
perthcantcode <ruperthayvesina1323@gmail.com>
2026-05-04 07:40:02 +0800

    Switch to render backend

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://cavsulit-production.up.railway.app/api' });
+const api = axios.create({ baseURL: 'https://cavsulit-backend.onrender.com/api' });
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('cavsulit_token');
