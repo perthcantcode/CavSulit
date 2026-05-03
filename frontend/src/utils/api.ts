66dd97dd16eac0c8d553b25b8b5100b22e4b96dc
perthcantcode <ruperthayvesina1323@gmail.com>
2026-05-04 00:44:01 +0800

    Connect frontend to Railway backend

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://cavsulit-production.up.railway.app/api' });
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('cavsulit_token');
